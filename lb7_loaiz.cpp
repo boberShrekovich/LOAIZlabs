555195a7ca155912a5a28a81a2779b73cef2b690
Denis <gureevdopp@mail.ru>
2024-11-19 14:51:40 +0300

    lab 7 osn zadanie

---
+++ lb7_loaiz.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int** createG(int size)
+{
+    int** G;
+    G = (int**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++)
+    {
+        G[i] = (int*)malloc(size * sizeof(int));
+    }
+    for (int i = 0; i < size; i++)
+    {
+        G[i][i] = 0;
+        for (int j = i + 1; j < size; j++)
+        {
+            G[i][j] = rand() % 2;
+            G[j][i] = G[i][j];
+        }
+    }
+    return G;
+}
+
+void printG(int** G, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            cout << G[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DFS(int** G, int size, int start, int* vis) {
+    vis[start] = 1;
+
+    cout << start << endl;
+
+    for (int i = 0; i < size; i++) {
+        if (G[start][i] == 1 && vis[i] == 0)
+            DFS(G, size, i, vis);
+    }
+
+
+}
+
+int main(){
+    int** G = NULL;
+    int nG, s;
+    
+
+    cout << "Vvedite razmer: ";
+    cin >> nG;
+
+    G = createG(nG);
+
+    printG(G, nG);
+    cout << endl;
+
+    cout << "Vvedite start: ";
+    cin >> s;
+    cout << endl;
+
+    int* vis = NULL;
+    vis = (int*)malloc(nG * sizeof(int));
+
+    for (int i = 0; i < nG; i++) {
+        vis[i] = 0;
+    }
+    
+    DFS(G, nG, s, vis);
+
+    return 0;
+}
